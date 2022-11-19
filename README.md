b12257d37484ddaf5c8b840082e2d1cca98d5ffb
MrZCookie <zacharyking08@hotmail.com>
2022-11-18 22:19:12 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cookie Clicker
+A simple cookie clicker game created by MrZCookie
+
+### Contributors
++ [MrZCookie](https://github.com/mrzcookies)
---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	var cookies = getCookies()
+
+	function getCookies() {
+		var amount;
+		var stored = localStorage.getItem('cookies')
+
+		if (stored === null) {
+			localStorage.setItem('cookies', 0)
+			amount = 0
+		} else {
+			amount = stored
+		}
+
+		return parseInt(amount)
+	}
+
+	function setCookies(amount) {
+		localStorage.setItem('cookies', parseInt(amount))
+	}
+
+	function addCookies(amount) {
+		setCookies(getCookies() + amount)
+		cookies = getCookies()
+	}
+
+	function removeCookies(amount) {
+		setCookies(getCookies() - amount)
+		cookies = getCookies()
+	}
+</script>
+
+<div class="flex justify-center items-center h-screen text-center">
+	<button on:click={() => {addCookies(1)}} class="pr-20">
+		<img src="cookie.png" alt="Cookie">
+	</button>
+	<h1 class="text-6xl text-white">{cookies} Cookies</h1>
+</div>
